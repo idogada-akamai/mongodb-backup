00ac46e425a529fe961f981f00c15a3a68ff0803
Ido Gada <igada@akamai.com>
2023-02-23 21:04:37 +0200

    storage systems in 1 file and fix attr del issue

--- rclone/rclone_config/storage_system.py
+++ rclone/rclone_config/storage_system.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class StorageSystem:
     name: str
     type: str
@@ -7,6 +10,50 @@ class StorageSystem:
         self.type = type
 
     def to_config_dict(self):
-        config_dict = self.__dict__
+        config_dict = deepcopy(self.__dict__)
         name = config_dict.pop("name")
-        return {name: config_dict}
+        return {self.name: config_dict}
+
+
+class S3(StorageSystem):
+    provider: str
+    access_key_id: str
+    secret_access_key: str
+    region: str
+    endpoint: str
+    type: str = "s3"
+
+    def __init__(
+        self,
+        provider: str,
+        access_key_id: str,
+        secret_access_key: str,
+        region: str,
+        endpoint: str,
+        name: str,
+    ) -> None:
+        self.provider = provider
+        self.access_key_id = access_key_id
+        self.secret_access_key = secret_access_key
+        self.region = region
+        self.endpoint = endpoint
+        StorageSystem.__init__(self, name=name, type=S3.type)
+
+
+class Http(StorageSystem):
+    url: str
+    type: str = "http"
+
+    def __init__(self, name: str, url: str) -> None:
+        self.url = url
+        StorageSystem.__init__(self, name=name, type=Http.type)
+
+
+def print_name(h: Http):
+    name = h.name
+    print(h.name)
+
+
+if __name__ == "__main__":
+    h = Http("test", "test")
+    print_name(h)
